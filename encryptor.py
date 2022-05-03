9ddeb51e6b4800ee3ebefbc53a73b65cc212f7da
Waseem <waseem.riaz.9999@gmail.com>
2022-05-03 23:42:57 +0530

    FInal Commit

--- encryptor.py
+++ encryptor.py
@@ -6,10 +6,10 @@ d = {} #a dictionary to assign each character a different value
 for smallCaps in (65,97): #loop a tuple for caps and small letters because ascii of A starts from 65 and a starts from 97 	 
 	for char in range(26): #loop from 0 to 25 to because 26 letters
 		d[chr((char+10)%26 + smallCaps)] = chr(char + smallCaps) #using chr to get the ascii corresponding value and then assigning it to a different letter in our dictionary
- 
-s = input("Enter yout message: ")
 
-print(''.join([d.get(char) for char in s])) #using join() to join the sequence from the dictionary.
+s = input("Enter your message: ")
+
+print(''.join([d.get(char,char) for char in s])) #using join() to join the sequence from the dictionary.
 						#here we iterate char over the input for each 
 						#letter and then use the dictionary to print
 						#the corresponding value to the key	
