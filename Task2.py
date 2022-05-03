d5311d6568346f836fa508b006797f7da9417225
Waseem <waseem.riaz.9999@gmail.com>
2022-05-03 23:41:26 +0530

    Finished with inputting the encoded message Errors: None Verdict: Success

--- Task2.py
+++ Task2.py
@@ -1,4 +1,18 @@
 """Task is to imitate a module like the zen module(Import this) """
 
-s = "Initial Test"
-print(s)
+s = """Unfuhyudsu yd Hkthq
+
+Jycu yd Hkthq xqi ruud luho fhetksjylu.Qtqhix rxqy yi qd unsubbudj juqsxuh.
+Jxekwx yj sqd ru xusjys qj jycui,y udzeo jxu jycu jxuhu buqhdydw dum jxydwi
+qdt tyisyfbydu.Qbie co fuuhi qhu huqbbo weet qdt admbutwuqrbu jxqj y wuj je buqhd q bej vhec
+jxuc. Y xefu y sqd sedjydku je ru yd jxyi juqc veh cehu jxqd 2 muuai
+
+"""
+
+d = {}
+
+for smallCaps in (65,97):
+	for char in range(26):
+		d[chr(char + smallCaps)] = chr((char + 10) % 26 + smallCaps)
+
+print(''.join([d.get(char,char) for char in s]))
